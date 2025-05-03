fb5f32c7788a4dd6e6a78f2effaecf8fb7a3ecdb
hamziAli <u2024203@giki.edu.pk>
2025-05-03 16:14:54 +0500

    Some functions done

--- GameComponent.cpp
+++ GameComponent.cpp
@@ -1,1 +1,56 @@
 // here we will do decelaration of the function and classes created in the header file
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include "GameComponent.h"
+using namespace std;
+// ===== Ship class =====
+Ship::Ship(string name, int size)
+{
+    this->name = name;
+    this->size = size;
+    hitsTaken = 0;
+    isHorizontal = true; // default
+    start = {0, 0};      // default
+}
+
+bool Ship::isSunk() const
+{
+    return hitsTaken >= size;
+}
+
+// ===== Board class =====
+
+Board::Board()
+{
+    // Allocate memory for grid
+    grid = new cell_status *[Board_Size];
+    for (int i = 0; i < Board_Size; ++i)
+    {
+        grid[i] = new cell_status[Board_Size];
+    }
+
+    // Initialize all cells to EMPTY
+    for (int row = 0; row < Board_Size; ++row)
+    {
+        for (int col = 0; col < Board_Size; ++col)
+        {
+            grid[row][col] = EMPTY;
+        }
+    }
+}
+
+Board::~Board()
+{
+    for (int i = 0; i < Board_Size; ++i)
+    {
+        delete[] grid[i];
+    }
+    delete[] grid;
+}
+int main()
+{
+
+    return 0;
+}
--- GameComponent.h
+++ GameComponent.h
@@ -1,2 +1,49 @@
 // Here we will add all the function prototypes of components of the game such as grid,
 // player vs player , player vs computer and ships
+// here we will do decelaration of the function and classes created in the header file
+#ifndef GAME_COMPONENT_H
+#define GAME_COMPONENT_H
+#include <vector>
+#include <string>
+using namespace std;
+const int Board_Size = 10;
+const int Ships = 9; // 5(king) , 4(Queen) , 3(Bishop) , 2(2xKnights) , 1(4xPawns)
+enum cell_status
+{
+    EMPTY,
+    SHIP,
+    HIT,
+    MISS
+};
+
+struct Coordinate
+{
+    int rows;
+    int cols;
+};
+
+class Ship
+{
+public:
+    string name;
+    int size;
+    Coordinate start; // Let’s say we have a 3-cell long ship placed horizontally at (row = 2, col = 4).
+    bool isHorizontal;
+    int hitsTaken;
+
+    Ship(string name, int size);
+    bool isSunk() const; // This is a member function of the Ship class that tells you whether the ship has been completely destroyed (sunk)
+};
+
+class Board
+{
+private:
+    cell_status **grid;
+    vector<Ship> Ship;
+
+public:
+    Board();
+    ~Board();
+};
+
+#endif
